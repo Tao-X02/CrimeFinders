f4fe8b8bef33a1d06d5b564d18cb525be72fc381
Taoguang Xing <taoguangxing@gmail.com>
2022-03-13 11:06:16 -0400

    added a few frontend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/home';
 import Dashboard from './Pages/dashboard';
+import SignUp from './Pages/signup';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard /> } />
+        <Route path="/signup" element={<SignUp /> } />
       </Routes>
     </BrowserRouter>
   );
